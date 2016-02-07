3707a1aee9572750be6c2ede6aa67ecc56349911
Olli Suoranta <ollisuoranta@gmail.com>
2016-02-07 21:06:18 +0200

    Great feel of accomplishment

--- trojan-defense-2D/code/game.js
+++ trojan-defense-2D/code/game.js
@@ -11,17 +11,23 @@ document.onreadystatechange = function () {
         });
 
         game.fullscreen = true;
+        var gridOffset = 144;
 
         var map1 = [
-            [1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 1],
-            [1, 0, 1, 0, 1],
-            [1, 0, 0, 0, 1],
-            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         ];
 
-        var mapBlockSizeX = 27;
-        var mapBlockSizeY = 37;
+        var mapBlockSizeX = 16;
+        var mapBlockSizeY = 16;
 
         var itemLayer = game.createLayer('items');
 
@@ -34,15 +40,27 @@ document.onreadystatechange = function () {
             for(var j = 0; j < mapBlock.length; j++) {
                 if (map1[i][j] == 1) {
                     var wall = itemLayer.createEntity();
-                    wall.size = { width: 47, height: 37 };
+                    wall.size = { width: 32, height: 32 };
 
                     // Isometric conversion
-                    wall.pos["x"] = 165 + currentBlockPosX - currentBlockPosY;
+                    wall.pos["x"] = gridOffset + currentBlockPosX - currentBlockPosY;
                     wall.pos["y"] = (currentBlockPosX + currentBlockPosY) / 2;
 
                     wall.asset = new PixelJS.Sprite();
                     wall.asset.prepare({
                         name: 'wall.png',
+                    });
+                } else {
+                    var floor = itemLayer.createEntity();
+                    floor.size = { width: 32, height: 32 };
+
+                    // Isometric conversion
+                    floor.pos["x"] = gridOffset + currentBlockPosX - currentBlockPosY;
+                    floor.pos["y"] = (currentBlockPosX + currentBlockPosY) / 2;
+
+                    floor.asset = new PixelJS.Sprite();
+                    floor.asset.prepare({
+                        name: 'floor.png',
                     });
                 }
                 currentBlockPosX = currentBlockPosX + mapBlockSizeX;
