b2bd8bfb9316281794f811588f058c93b37e19a7
Olli Suoranta <ollisuoranta@gmail.com>
2016-02-08 22:52:33 +0200

    Add backdoors, file, and try to implement zindex stuff

--- trojan-defense-2D/README.md
+++ trojan-defense-2D/README.md
@@ -2,16 +2,7 @@
 Once upon a time there was a unfinished game called Trojan Defense (which is still in developement).
 It was/is a 3D FPS. This project is about making a 2D version of it with HTML5/Javascript and some other random libraries.
 
-Todo:
-- GUI
-- Character movement (keys to move, mouse to aim)
-- Level generation / Wall placement
-- Pathfinding + Trojan AI
-- Inserting obstacles
-- Player class to handle player attributes (HP, items, ammoes,..)
-- Changing weapon functions
-- Spawning trojans (enemies)
-- Inventory / Powerup system
-- Excellent particle effects and bulletholes
-- Darkness behind corners
-- Sound effects & Music
+![alt tag](https://raw.githubusercontent.com/PHILPANDA/pandatom/master/trojan-defense-2d/screenshots/current.png)
+(Screenshot of current status)
+
+This game is in development.
--- trojan-defense-2D/code/game.js
+++ trojan-defense-2D/code/game.js
@@ -1,3 +1,5 @@
+// Copyright Pandatom 2016
+
 document.onreadystatechange = function () {
     if (document.readyState == "complete") {
 
@@ -6,24 +8,29 @@ document.onreadystatechange = function () {
         // Init settings
         game.init({
             container: 'game_container',
-            width: 320,
-            height: 200
+            width: 640,
+            height: 480
         });
 
         game.fullscreen = true;
-        var gridOffset = 144;
+        var gridOffset = 260;
 
+        // 0 = floor, 1 = wall, 3 = backdoor, 4 = file
         var map1 = [
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 1, 1, 0, 0, 1, 1, 1, 1],
-            [1, 0, 0, 0, 0, 0, 1, 0, 1, 1],
-            [1, 0, 0, 0, 0, 0, 1, 0, 1, 1],
-            [1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1],
+            [1, 0, 0, 0, 0, 0, 1, 0, 0, 4, 0, 1, 0, 1, 1],
+            [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 0, 4, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         ];
 
         var mapBlockSizeX = 16;
@@ -35,12 +42,16 @@ document.onreadystatechange = function () {
 
         var wallArray = [];
         var floorArray = [];
+        var backdoorArray = [];
+        var fileArray = [];
 
         var currentBlockPosX = mapBlockSizeX;
         var currentBlockPosY = mapBlockSizeY;
+        var zIndex = 0;
 
         for(var i = 0; i < map1.length; i++) {
             var mapBlock = map1[i];
+            zIndex++;
 
             for(var j = 0; j < mapBlock.length; j++) {
                 if (map1[i][j] == 1) {
@@ -52,12 +63,49 @@ document.onreadystatechange = function () {
                     wall.pos["x"] = gridOffset + currentBlockPosX - currentBlockPosY;
                     wall.pos["y"] = (currentBlockPosX + currentBlockPosY) / 2;
 
+                    wall.zIndex = zIndex;
+
                     wall.asset = new PixelJS.Sprite();
                     wall.asset.prepare({
                         name: 'wall.png',
                     });
 
                     wallArray.push(wall);
+
+                } else if (map1[i][j] == 3) {
+                    var backdoor = itemLayer.createEntity();
+                    backdoor.size["width"] = blockRange;
+                    backdoor.size["height"] = blockRange;
+
+                    // Isometric conversion
+                    backdoor.pos["x"] = gridOffset + currentBlockPosX - currentBlockPosY;
+                    backdoor.pos["y"] = (currentBlockPosX + currentBlockPosY) / 2;
+
+                    backdoor.zIndex = zIndex;
+
+                    backdoor.asset = new PixelJS.Sprite();
+                    backdoor.asset.prepare({
+                        name: 'backdoor.png',
+                    });
+
+                    backdoorArray.push(backdoor);
+                } else if (map1[i][j] == 4) {
+                    var file = itemLayer.createEntity();
+                    file.size["width"] = blockRange;
+                    file.size["height"] = blockRange;
+
+                    // Isometric conversion
+                    file.pos["x"] = gridOffset + currentBlockPosX - currentBlockPosY;
+                    file.pos["y"] = (currentBlockPosX + currentBlockPosY) / 2;
+
+                    file.zIndex = zIndex;
+
+                    file.asset = new PixelJS.Sprite();
+                    file.asset.prepare({
+                        name: 'file.png',
+                    });
+
+                    fileArray.push(file);
                 } else {
                     var floor = itemLayer.createEntity();
                     floor.size["width"] = blockRange;
@@ -66,6 +114,8 @@ document.onreadystatechange = function () {
                     // Isometric conversion
                     floor.pos["x"] = gridOffset + currentBlockPosX - currentBlockPosY;
                     floor.pos["y"] = (currentBlockPosX + currentBlockPosY) / 2;
+
+                    floor.zIndex = zIndex;
 
                     floor.asset = new PixelJS.Sprite();
                     floor.asset.prepare({
@@ -85,11 +135,12 @@ document.onreadystatechange = function () {
 
         player.addToLayer(playerLayer);
 
-        player.pos = { x: 100, y: 100 };
+        player.pos = { x: 150, y: 100 };
         player.size["width"] = playerRange;
         player.size["height"] = playerRange;
         player.velocity = { x: 100, y: 100 };
         player.asset = new PixelJS.AnimatedSprite();
+        player.zIndex = 1;
         player.asset.prepare({
             name: 'char.png',
             frames: 3,
@@ -99,6 +150,13 @@ document.onreadystatechange = function () {
         });
 
         player.onCollide(function (entity) {
+            floorArray.forEach(function(entry) {
+                if (entity === entry) {
+                    console.log("player: " + player.zIndex);
+                    console.log("entry: " + entry.zIndex);
+                }
+            });
+
             // @todo This is way too heavy, implement better later
             wallArray.forEach(function(entry) {
                 if (entity === entry) {
@@ -139,6 +197,10 @@ document.onreadystatechange = function () {
         });
 
         playerLayer.registerCollidable(player);
+
+        floorArray.forEach(function(entry) {
+            itemLayer.registerCollidable(entry);
+        });
 
         wallArray.forEach(function(entry) {
             itemLayer.registerCollidable(entry);
