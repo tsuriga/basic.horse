d0309906d3387e67378e47ff97a3aee15610fc02
Olli Suoranta <ollisuoranta@gmail.com>
2016-03-22 21:17:32 +0200

    change the starting point of bullet

--- trojan-defense-2D/code/game.js
+++ trojan-defense-2D/code/game.js
@@ -51,7 +51,7 @@ function shootFrom(player) {
 
     getFreeAudio().play();
     bullet.pos.x = player.pos.x + 9;
-    bullet.pos.y = player.pos.y + 9;
+    bullet.pos.y = player.pos.y + 20;
     bullet.visible = true;
 
     switch (bullet.direction) {
