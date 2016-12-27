5ae3a25f6587a5e065d1cf22fd87dc926abaa644
Olli Suoranta <ollisuoranta@gmail.com>
2016-12-27 02:03:44 +0200

    add license stuff

--- games/ghost-defense/code/game.js
+++ games/ghost-defense/code/game.js
@@ -1,11 +1,20 @@
-/*
- * Ghost Defense 2D
- * Copyright (C) 2016 Basic Horse (Olli Suoranta and Juho Saarelainen)
- *
- * This is the first game of Basic Horse.
- *
- * Version 0.9
- */
+// Copyright (C) 2016 Basic Horse (Olli Suoranta and Juho Saarelainen)
+//
+// Version 1.0
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
 
 const GAME_WIDTH = 640;
 const GAME_HEIGHT = 350;
